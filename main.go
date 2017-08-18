b1cecc183131fc08897fd83c41aabd0467f4513d
Ke Xu <kdotxu@gmail.com>
2017-08-18 09:28:39 -0400

    log to file

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 )
 
 func main() {
+	f, err := os.OpenFile("debug.log", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
 
 	host := flag.String("host", "aardmud.org", "mud server host")
 	port := flag.String("port", "4000", "mud server port")
